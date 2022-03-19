183d48c2cfe4ebede055f7d02addd4e08d664ce7
Jorge Gallegos <a1121250017@utch.edu.mx>
2022-03-18 22:37:11 -0700

    Primera subida

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { View, Text, Button, SafeAreaView } from "react-native";
 
 export default function SettingsScreen({ route, navigation }) {
    const { itemId } = route.params;
+   const [post, setPost] = useState([]);
+
+   const getPost = async() => {
+    try{
+      const url = `https://jsonplaceholder.typicode.com/posts/${itemId}`;
+      const response = await fetch(url); //consumir los datos
+      const json = await response.json(); //convertir a json
+      setPost(json);
+      console.log(url)
+    } catch (error) {
+      console.error(error);
+
+    }
+  }
+
+  useEffect(()=>{
+    getPost()
+  }, [post])
+
+
   return (
     <SafeAreaView>
-      <Text>Detalle {JSON.stringify(itemId)}</Text>
+      <Text>Id: {JSON.stringify(itemId)}</Text>
+      <Text>Titulo: {post.title}</Text>
+      <Text>Body: {post.body}</Text>
+
       <Button onPress={() =>{
         navigation.navigate('Home');
       }} title="REGRESAR" />
+
     </SafeAreaView>
   );
 }
